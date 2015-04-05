5fe57eb5d4cff09e9f5ce442abb2e2f0c6e6a93d
Mike Axford <mike@m1ari.co.uk>
2015-04-05 18:17:33 +0000

    Some tidying, get node and location from config, allow for non json mode (lightweight mode)

--- pinode.c
+++ pinode.c
@@ -20,6 +20,24 @@
 #include "RFM69.h"
 #include "RFM69Config.h"
 
+
+/* Options to edit are here
+ * DEBUG_SPI makes it print lots of debug stuff
+ * JSON_CONFIG uses config.json (should be on by default
+ */
+
+//#define DEBUG_SPI	// Debug SPI stuffs
+#define JSON_CONFIG	// Config is in config.json
+
+/* If you disabled JSON_CONFIG then set values here
+ */
+#ifndef JSON_CONFIG
+  #define SPI_DEV "/dev/spidev0.1"
+  #define NODE_NAME "MAPI"
+  #define LOCATION "0.1,0.1,100"
+#endif
+
+
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 void spi_set_mode(int fd, uint8_t mode){
@@ -88,71 +106,44 @@ void spi_get_state(int fd){
 	printf("bits per word: %d\n", bits);
 	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
 }
-//static uint16_t delay;
 
 int spi_transfer (int fd, uint8_t *data, int len) {
-  struct spi_ioc_transfer spi ;
+	struct spi_ioc_transfer spi ;
+	int ret;
+
+#ifdef DEBUG_SPI
 	int i;
 	printf("SPI Sending:  ");
 	for (i=0; i<len; i++)
 		printf("%02X ", data[i]);
 	printf("\n");
+#endif
 
 	memset (&spi, 0, sizeof (spi)) ;
 
 	spi.tx_buf        = (unsigned long)data ;
 	spi.rx_buf        = (unsigned long)data ;
 	spi.len           = len ;
-	spi.delay_usecs   = 0 ;
-	spi.speed_hz      = 0 ;
-	spi.bits_per_word = 0 ;
+	//spi.delay_usecs   = 0 ;
+	//spi.speed_hz      = 0 ;
+	//spi.bits_per_word = 0 ;
 
-	int ret= ioctl (fd, SPI_IOC_MESSAGE(1), &spi) ;
-
-	printf("SPI Recieved: ");
-	for (i=0; i<len; i++)
-		printf("%02X ", data[i]);
-	printf("\n");
+	ret= ioctl (fd, SPI_IOC_MESSAGE(1), &spi) ;
+	if (ret < 1){
+		perror("Error sending SPI Message");
+#ifdef DEBUG_SPI
+	} else {
+		printf("SPI Recieved: ");
+		for (i=0; i<len; i++)
+			printf("%02X ", data[i]);
+		printf("\n");
+#endif
+	}
 
 	return ret;
 }
 
-/*
-static void transfer(int fd)
-{
-	int ret;
-	uint8_t tx[] = {
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0x40, 0x00, 0x00, 0x00, 0x00, 0x95,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xDE, 0xAD, 0xBE, 0xEF, 0xBA, 0xAD,
-		0xF0, 0x0D,
-	};
-	uint8_t rx[ARRAY_SIZE(tx)] = {0, };
-	struct spi_ioc_transfer tr = {
-		.tx_buf = (unsigned long)tx,
-		.rx_buf = (unsigned long)rx,
-		.len = ARRAY_SIZE(tx),
-		.delay_usecs = delay,
-		.speed_hz = speed,
-		.bits_per_word = bits,
-	};
-
-	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-	if (ret < 1)
-		pabort("can't send spi message");
-
-	for (ret = 0; ret < ARRAY_SIZE(tx); ret++) {
-		if (!(ret % 6))
-			puts("");
-		printf("%.2X ", rx[ret]);
-	}
-	puts("");
-}
-*/
-
+/* Reads a single byte from a register */
 uint8_t spi_read(int fd, uint8_t reg){
 	uint8_t buffer[2];
 	buffer[0] = reg & ~RFM69_SPI_WRITE_MASK;	// Ensure the write mask is off
@@ -161,6 +152,7 @@ uint8_t spi_read(int fd, uint8_t reg){
 	spi_transfer(fd, buffer, ARRAY_SIZE(buffer)); // Send the address with the write mask off
 	return buffer[1];
 }
+/* Writes a single byte to a register */
 void spi_write(int fd, uint8_t reg, uint8_t val){
 	uint8_t buffer[2];
 	buffer[0] = reg | RFM69_SPI_WRITE_MASK;	// Set the write mask
@@ -175,6 +167,7 @@ void rfm69_setmode(int fd, uint8_t mode){
 
 int main(int argc, char *argv[]) {
 
+#ifdef JSON_CONFIG
 	/* Load the config.json file */
 	FILE *fp_json=NULL;
 	json_error_t err;
@@ -194,9 +187,9 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
+	/* get the SPI device from config.json */
 	json_t *jdev;
 	jdev = json_object_get(config, "device");
-	
 	const char *device;
 	if (json_is_string(jdev)){
 		device=json_string_value(jdev);;
@@ -204,6 +197,72 @@ int main(int argc, char *argv[]) {
 		perror("Can't read device name from config file\n");
 		exit(-1);
 	}
+
+	/* Get the nodename from config.json */
+	json_t *jnode;
+	jnode = json_object_get(config, "nodename");
+	const char *nodename;
+	if (json_is_string(jnode)){
+		nodename=json_string_value(jnode);;
+	} else {
+		perror("Can't read nodename name from config file\n");
+		exit(-1);
+	}
+
+	/* get the location from config.json */
+	char location[31];	// lat: -nnn.nnnnn, long: -nn.nnnnn, Alt: -nnnnn.nn (10 + 9 + 9 + commas)
+	json_t *jloc;
+	jloc = json_object_get(config, "location");
+	if (!json_is_object(jloc)){
+		perror("Can't read location from config file\n");
+		exit(-1);
+	}
+
+	double latitude, longitude;
+	json_t *jloc_lat, *jloc_lon, *jloc_alt;
+
+	jloc_lat = json_object_get(jloc, "latitude");
+	if (json_is_real(jloc_lat)){
+		latitude=json_real_value(jloc_lat);
+	} else {
+		perror("Can't read latitude from config file\n");
+		exit(-1);
+	}
+
+	jloc_lon = json_object_get(jloc, "longitude");
+	if (json_is_real(jloc_lat)){
+		longitude=json_real_value(jloc_lon);
+	} else {
+		perror("Can't read longitude from config file\n");
+		exit(-1);
+	}
+
+	jloc_alt = json_object_get(jloc, "altitude");
+	if (json_is_real(jloc_alt)){
+		double altitude;
+		altitude=json_real_value(jloc_alt);
+		snprintf(location,31,"%.5f,%.5f,%.2f",latitude, longitude, altitude);
+	} else if (json_is_integer(jloc_alt)){
+		int altitude;
+		altitude=json_integer_value(jloc_alt);
+		snprintf(location,31,"%.5f,%.5f,%d",latitude, longitude, altitude);
+	} else {
+		snprintf(location,31,"%.5f,%.5f",latitude, longitude);
+		perror("Can't read altitude from config file\n");
+	}
+	json_decref(jloc_lat);
+	json_decref(jloc_lon);
+	json_decref(jloc_alt);
+	json_decref(jloc);
+
+
+
+#else
+	/* if we're not using config.json set the same variables to use the #define values */
+	static const char *device = SPI_DEV;
+	static const char *nodename = NODE_NAME;
+	static const char *location = LOCATION;
+#endif
 	
 	fprintf(stdout,"Opening SPI Device on %s\n", device);
 
@@ -214,7 +273,7 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 	/* If we're not going to use device anymore we should decref it
-	 * json_decref(jdev);
+	 * json_decref(jdev) but only if it came from json;
 	 */
 
 	spi_get_state(fd);
@@ -237,7 +296,7 @@ int main(int argc, char *argv[]) {
 
 
 	char string[65];
-	snprintf(string,65,"3aL50.944190,-1.40550T19[MAPI]");
+	snprintf(string,65,"3aL%sT19[%s]",location,nodename);
 
 	uint8_t buffer[67];
 	buffer[0] = 0x80; // Fifo mode
@@ -246,23 +305,17 @@ int main(int argc, char *argv[]) {
 
 	// Send Packet
 	rfm69_setmode(fd, RFM69_MODE_TX);
-	uint8_t val;
 	while(!(spi_read(fd, RFM69_REG_27_IRQ_FLAGS1) & RF_IRQFLAGS1_TXREADY)) { };
-/*
-	while(1){
-		val=spi_read(fd, RFM69_REG_27_IRQ_FLAGS1);
-		printf("Reg27 Wait: Got %02X want %02X\n", val,RF_IRQFLAGS1_TXREADY);
-		if (val & RF_IRQFLAGS1_TXREADY)
-			break;
-	}
-*/
+
 	spi_transfer(fd,buffer,buffer[1]+2);
 	while(!(spi_read(fd,RFM69_REG_28_IRQ_FLAGS2) & RF_IRQFLAGS2_PACKETSENT)) { };
 
 	rfm69_setmode(fd,RFM69_MODE_RX);
 
 	close(fd);
+#ifdef JSON_CONFIG
 	json_decref(config);
+#endif
 	return 0;
 } 
 
